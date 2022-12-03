4782c104cb4a43e967e2df896bb8fedd34440a09
Roman-Ojha <razzroman98@gmail.com>
2022-12-03 14:24:22 +0545

    feat: added index on some models

---
+++ prisma/migrations/20221203083656_added_index_on_genere_and_book_review_and_store_book_review/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "BookReview_rate_idx" ON "BookReview"("rate");
+
+-- CreateIndex
+CREATE INDEX "Genere_name_idx" ON "Genere"("name");
+
+-- CreateIndex
+CREATE INDEX "StoreBookReview_rate_idx" ON "StoreBookReview"("rate");
